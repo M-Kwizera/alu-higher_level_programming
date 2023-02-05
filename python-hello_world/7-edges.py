ce1677eae82c3ff9c770cf551e7e1aab46632a10
M-Kwizera <m.kwizera@alustudent.com>
2023-02-05 02:50:22 -0800

    Modification made to file ...

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = "Holberton"
 word_last_2 = "Holberton"
-middle_word = "Holberton"
+middle_word = word[1:len(word)-1]
 print(f"First 3 letters: {word_first_3[:3]}")
 print(f"Last 2 letters: {word_last_2[-2:]}")
-print(f"Middle word: {middle_word[1:len(word)-1]")
+print(f"Middle word: {middle_word}")
