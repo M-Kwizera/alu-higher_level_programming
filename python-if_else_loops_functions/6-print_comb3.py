d35fc28690a054e5f698f61e0ae7c6ad2d6e325d
Maxime <m.kwizera@alustudent.com>
2023-02-08 14:05:35 +0200

    modified 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 9 and j == 9:
-            print("{}{}".format(i, j))
+for i in range(0, 9):
+    for j in range(i, 10):
+        if i == j:
+            continue
+        if i == 8 and j == 9:
+            print("{}".format(str(i) + str(j)))
+        elif int(str(i) + str(j)) < int(str(j) + str(i)):
+            print("{}, ".format(str(i) + str(j)), end="")
         else:
-            print("{}{}, ".format(i, j), end="")
+            print("{}, ".format(str(j) + str(i)), end="")
