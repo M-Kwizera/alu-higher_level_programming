72cef0a207b2aebba4698d55eb556dc642c33138
M-Kwizera <m.kwizera@alustudent.com>
2023-02-21 02:49:53 -0800

    committed number ...

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from add_0.py import *
+print('1+2=3')
---
+++ python-import_modules/README.py
@@ -0,0 +1,1 @@
+file 0-add.py imports the function def add(a,b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
