6555cc4ccacd01700d27396df0997f246c2b2f54
M-Kwizera <m.kwizera@alustudent.com>
2023-03-02 05:43:00 -0800

    added file 0 and README

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    count = 0
+    while count < len(my_list):
+        print(count)
+        count += 1
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+file 0 prints integers in a list
