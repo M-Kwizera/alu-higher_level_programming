1daccc6fda2a5afcf19250702715b8270e256d68
M-Kwizera <m.kwizera@alustudent.com>
2023-03-08 00:18:31 -0800

    6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dictionary = sorted(a_dictionary)
     for key_1 in sorted_dictionary:
-        print(F'{temp}: {a_dictionary[temp]}')
+        print(F'{key_1}: {a_dictionary[key_1]}')
         
