61c0513e8535b5be37aa83df4c829ad2ce24214f
M-Kwizera <m.kwizera@alustudent.com>
2023-03-09 01:30:57 -0800

    modification to 0-...

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while my_list[count] and count < x:
-            print(my_list[count], end=" ")
+        while count < x:
+            print(my_list[count], end="")
             count += 1
-    except:
+    except IndexError:
         pass
     finally:
         print()
-    return count
+        return count
