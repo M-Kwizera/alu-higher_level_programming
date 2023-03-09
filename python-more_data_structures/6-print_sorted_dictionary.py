6c1688828636f4cc8106653a6472bd32413e3cb6
M-Kwizera <m.kwizera@alustudent.com>
2023-03-09 01:08:05 -0800

    minor edits to files 3, 6 ,12 ,13

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,5 +4,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dictionary = sorted(a_dictionary)
     for key_1 in sorted_dictionary:
-        print(F'{key_1}: {a_dictionary[key_1]}')
-        
+        print(F'{key_1}: {a_dictionary[key_1]}')        
--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -6,4 +6,4 @@ def simple_delete(a_dictionary, key=""):
         del a_dictionary[key]
         return a_dictionary
     else:
-        return 
+        return a_dictionary 
