b82a6dabcf9bd5b0e52a2e7d66643b3503cb092f
M-Kwizera <m.kwizera@alustudent.com>
2023-04-04 11:02:01 +0000

    4-headers.sh correction

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take in a URL as an argument sned a get request and display the body of the response
-curl -sHG 'X-HolbertonSchool-User-Id: 98' "$1" 
+curl -sG "$1" -H "X-School-User-Id: 98"
