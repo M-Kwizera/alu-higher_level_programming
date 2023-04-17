629233e87d4fd0b8e9e9520c77f32c0345e25678
Maxime <m.kwizera@alustudent.com>
2023-04-17 14:12:52 +0200

    clear

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# This function 'add_integer' takes in two arguments and returns the sum of the two.
+""" This function 'add_integer' takes in two arguments """
+"""and returns the sum of the two."""
+
+
 def add_integer(a, b=98):
     """Should the conditions be met, Raising the type error message"""
     if type(a) != int and type(a) != float:
