87d7d6290a8b1b06e498958196054c5da4e92762
Maxime <m.kwizera@alustudent.com>
2023-04-17 13:51:06 +0200

    changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 # This function add_integer takes in two arguments and returns the sum of them
+# def add_integer(a, b=98):
+#     """
+#     add_integer takes in two arguments
+#     checks whether argument (a) and (b) are of integer type
+#     if they are not integers raises TypError 'must be an integer' or 'b must be an integer'
+#     Proceeds with casting a and b as integers
+#     Returns a + b
+#     """
+#     if not isinstance(a, int):
+#         raise TypeError("a must be an integer")
+#     if not isinstance(b, int):
+#         raise TypeError("b must be an integer")
+#     a = int(a)
+#     b = int(b)
+#     return a + b
+
 def add_integer(a, b=98):
-    """
-    add_integer takes in two arguments
-    checks whether argument (a) and (b) are of integer type
-    if they are not integers raises TypError 'must be an integer' or 'b must be an integer'
-    Proceeds with casting a and b as integers
-    Returns a + b
-    """
-    if not isinstance(a, int):
+    """ Should the conditions be met, Raising the type error message"""
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
-
-
-print(add_integer(1, 2))
+    return int(a) + int(b)
