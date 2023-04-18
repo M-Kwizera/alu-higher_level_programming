446401b4ccae2c751d0ab611d19701dfd1a7e0e2
Maxime <m.kwizera@alustudent.com>
2023-04-18 11:57:54 +0200

    sa

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,9 +7,10 @@
 def say_my_name(first_name, last_name):
     """Checking if first_name and last_name
         are strings."""
-    if type(first_name) != str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     calling = (F"My name is {first_name} {last_name}")
     return calling
