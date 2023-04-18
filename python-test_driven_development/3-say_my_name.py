9febc1f3d97d3eda2e4ae4648ee9801916416136
Maxime <m.kwizera@alustudent.com>
2023-04-18 12:21:24 +0200

    added a slash

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,8 @@ def say_my_name(first_name, last_name):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     f_missing = (
-        "Traceback (most recent call last):\n...\nTypeError: say_my_name() missing 1 required positional argument: 'first_name'")
+        "Traceback (most recent call last):\n...\nTypeError: say_my_name()"
+        "missing 1 required positional argument: 'first_name'")
     if first_name is None:
         raise TypeError(f_missing)
 
