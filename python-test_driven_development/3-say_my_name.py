b1e9d39c3e000eb54b80d32cdbd048fe0a108f60
Maxime <m.kwizera@alustudent.com>
2023-04-18 09:34:43 +0200

    3-say-my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -13,6 +13,3 @@ def say_my_name(first_name, last_name):
         raise TypeError("last_name must be a string")
     calling = (F"My name is {first_name} {last_name}")
     return calling
-
-
-print(say_my_name("John", "Smith"))
