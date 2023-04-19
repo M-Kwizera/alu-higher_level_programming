863bfdb1c67238cdb879e9c74a9903d42fdfe77e
Maxime <m.kwizera@alustudent.com>
2023-04-19 10:40:35 +0200

    a

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,8 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be an integer")
+        raise ValueError("Traceback (most recent call last):"
+                         "size must be >= 0")
     if isinstance(size, float) < 0:
         raise TypeError("size must be an integer")
 
