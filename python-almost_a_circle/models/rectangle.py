0fdf0af60b0f34e7d8ab958d05bf39e0c1b1fc28
Maxime <m.kwizera@alustudent.com>
2023-04-24 07:31:36 +0200

    rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+"""Rectangle inherits from Base"""
+
+Base = __import__("base").Base
+
+
+class Rectangle(Base):
+
+    """Class Rectangle inherits from class base
+    that is in the same directory(base.py)."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """rectangle class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, a):
+        """Width setter"""
+        if type(a) != int:
+            raise TypeError("width must be an integer")
+        if a <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = a
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, a):
+        """set height"""
+        if type(a) != int:
+            raise TypeError("height must be an integer")
+        if a <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = a
