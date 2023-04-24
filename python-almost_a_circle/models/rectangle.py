aa81fdae4f58244df24af3311b38949c3018c4e8
Maxime <m.kwizera@alustudent.com>
2023-04-24 07:37:22 +0200

    a

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""A rectangular module with rectangle class"""
 from models.base import Base
 
-"""Rectangle inherits from Base"""
+"""Rectangle class inherits from Base"""
 
 # Base = __import__("base").Base
 
