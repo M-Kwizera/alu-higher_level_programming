6cc767f4f1a10ce2795c0b16a756ea1c3f15a5e7
Maxime <m.kwizera@alustudent.com>
2023-04-24 07:34:16 +0200

    a

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+import base
 
 """Rectangle inherits from Base"""
 
-Base = __import__("base").Base
+# Base = __import__("base").Base
 
 
-class Rectangle(Base):
+class Rectangle(base.Base):
 
     """Class Rectangle inherits from class base
     that is in the same directory(base.py)."""
