1d582cc01670222cbf7bf8c5400107cfc70eaabe
Maxime <m.kwizera@alustudent.com>
2023-04-24 07:39:49 +0200

    b

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(Base):
     that is in the same directory(base.py)."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """rectangle class"""
+        """class Initialisor/constructor"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -47,3 +47,81 @@ class Rectangle(Base):
         if a <= 0:
             raise ValueError("height must be > 0")
         self.__height = a
+
+    @property
+    def x(self):
+        """set x"""
+        return self.__x
+
+    @x.setter
+    def x(self, a):
+        """set x"""
+        if type(a) != int:
+            raise TypeError("x must be an integer")
+        if a < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = a
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, a):
+        """Setter y"""
+        if type(a) != int:
+            raise TypeError("y must be an integer")
+        if a < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = a
+
+    def area(self):
+        """Calculating the Area"""
+        return (self.__height * self.__width)
+
+    def display(self):
+        """Display our output"""
+        print('\n' * self.__y, end='')
+        for i in range(self.__height):
+            print(' ' * self.__x + '#' * self.__width)
+
+    def __str__(self):
+        """String showcase of our arguments"""
+        o = self.id
+        p = self.__x
+        r = self.__y
+        s = self.__width
+        t = self.__height
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
+                format(o, p, r, s, t))
+
+    def update(self, *args, **kwargs):
+        """take multi update"""
+        dct = {}
+        if args is not None and len(args) > 0:
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args) if len(args) <= 5 else 5):
+                dct[keys[i]] = args[i]
+        else:
+            dct = kwargs
+
+        if len(dct) > 0:
+            for key, a in dct.items():
+                if key == "id" and a is None:
+                    p = self.__width
+                    r = self.__height
+                    s = self.__x
+                    self.__init__(p, r, s, self.__y)
+                else:
+                    setattr(self, key, a)
+
+    def to_dictionary(self):
+        """dictionary formatting"""
+        dict = {}
+        dict["id"] = self.id
+        dict["width"] = self.width
+        dict["height"] = self.height
+        dict["x"] = self.x
+        dict["y"] = self.y
+        return (dict)
