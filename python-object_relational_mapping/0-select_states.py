0666fcd02866294ec7e6d3854862a40cfac83ec2
Maxime <m.kwizera@alustudent.com>
2023-04-26 09:04:41 +0200

    a$

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that lists all states from htbn"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    # port and host are default local and 3306
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    result = cur.fetchall()
+    for i in result:
+        print(i)
+    # close cursor and db
+    cur.close()
+    db.close()
