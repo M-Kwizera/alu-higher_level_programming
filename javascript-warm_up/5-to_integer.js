98bc02775ba158523c33be70c97734c8299cbbe4
Maxime <m.kwizera@alustudent.com>
2023-06-13 13:48:12 +0200

    5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const arg = process.argv;
-const conversion = Number(arg[2]);
-if (conversion) {
-    console.log(`My number: ${arg[2]}`);
-} else {
-    console.log('Not a number');
+const {argv} = process;
+const nArg = Number(argv[2]);
+if (!nArg) console.log('Not a number');
+else {
+    console.log('My number:', nArg);
 }
