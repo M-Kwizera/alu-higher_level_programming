eb8a910bdb388f17aa65587a36e5443f57dfcbc7
Maxime <m.kwizera@alustudent.com>
2023-06-13 16:20:44 +0200

    added 6..

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const line_s = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// console.log(line_s.length)
+
+
+for (let i = 0; i < line_s.length; i++) {
+    console.log(line_s[i]);
+}
