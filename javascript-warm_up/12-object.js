91cdd6bc1f97d066c64c6d764f714b0c5dc876aa
Maxime <m.kwizera@alustudent.com>
2023-06-14 12:34:47 +0200

    added  12-..

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+YOUR CODE HERE
+*/
+myObject = {
+  type: 'object',
+  value: 89
+};
+
+console.log(myObject);
